d4b42f7020802875db7d515f79a05f5820fa89b3
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-12 16:16:37 -0300

    Adição de comentários no código

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const app = express();
 app.use(express.json()); //habilita que o express trabalhe com JSON
 const users = [];
 
+//Criacao do method POST com express
 app.post("/users", (req, res) => {
     const body = req.body;
     users.push(body);
     res.status(201).send("User successfully created");
 });
 
+//Criacao do method GET com express
 app.get("/users", (req, res) => {
     res.send({message: "This is users:", users});
 })

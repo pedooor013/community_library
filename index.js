3b40d1d15069c3358a1c613db40a773da2a20371
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-15 16:24:07 -0300

    addd: adição do user.routes no index.js

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 import express from 'express'
+import userRouters from './src/routes/user.routes.js'
 
 const app = express();
 
 app.use(express.json()); //habilita que o express trabalhe com JSON
-const users = [];
-
-//Criacao do method POST com express
-app.post("/users", (req, res) => {
-    const body = req.body;
-    users.push(body);
-    res.status(201).send("User successfully created");
-});
-
-//Criacao do method GET com express
-app.get("/users", (req, res) => {
-    res.send({message: "This is users:", users});
-})
-
+app.use(userRouters); 
 
 app.listen(3000, () => {
     console.log(`Server is running in port 3000`);

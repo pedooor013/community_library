25c80424e9dd9dc731bbf67e91eba74d7474584a
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-15 16:20:26 -0300

    addd: criação do router com express

---
+++ src/routes/user.routes.js
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+import userControllers from '../controller/user.controllers.js';                                                    
+
+const router = Router();
+
+router.post('/users', userControllers.createUserController);
+
+export default router;

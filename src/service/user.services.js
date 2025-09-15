9454bb8a9d6870bf224fc28f112dc3e6b8515dc1
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-15 19:09:32 -0300

    addd: criptografia da senha

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,9 +1,17 @@
 import userRepository from '../repositories/user.repositories.js';
+import bcrypt from 'bcrypt';
+
 
 async function createUserService(newUser){
     const foundUser = await userRepository.findUserByEmailRepository(newUser.email);
     if(foundUser) throw new Error("User already exists!");
-    const user = await userRepository.createUserRepository(newUser);
+    
+    const passHash = await bcrypt.hash(newUser.password, 10);
+    const user = await userRepository.createUserRepository({...newUser,
+        password: passHash,
+    });
+    if(!user) throw new Error("Error creating user!");
+
     return user;
 }
 

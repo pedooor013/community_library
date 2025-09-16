baa04ce461bc5e043fa9ed511fe1d3bc3362022f
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-16 15:52:40 -0300

    addd: Criação do middleware para a rota de users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import userControllers from '../controller/user.controllers.js';                                                    
+import {validate} from '../middlewares/validation.middlewares.js';
+import { userSchema } from '../schema/user.schema.js';
 
 const router = Router();
 
-router.post('/users', userControllers.createUserController);
+router.post('/users', validate(userSchema) , userControllers.createUserController);
 
 export default router;

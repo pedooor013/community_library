74e2cf6f23ff9d7f936411351482d70317819e60
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-16 15:36:52 -0300

    addd: criação do user.schema para validação dos dados inseridos para o DB

---
+++ src/schema/user.schema.js
@@ -0,0 +1,10 @@
+import {z} from 'zod';
+
+const userSchema = z.object({
+    username: z.string().min(3, 'Username is required'),
+    email: z.string.email('Invalid email'),
+    password: z.string.min(6, 'Password is required'),
+    avatar: z.string.url('Invalid URL').optional()
+});
+
+export {userSchema};

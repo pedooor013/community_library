ba8819c51f172ddb012f52e3cc602e2a59f0d59e
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-22 20:03:30 -0300

    addd: autentificação para o usuário fazer algumas ações no banco de dados

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,12 +2,15 @@ import {Router} from 'express';
 import userControllers from '../controller/user.controllers.js';                                                    
 import {validate, validateUserID} from '../middlewares/validation.middlewares.js';
 import { userSchema } from '../schema/user.schema.js';
+import { authMiddleware } from '../middlewares/auth.middlewares.js';
 
 const router = Router();
 
 router.post('/users', validate(userSchema) , userControllers.createUserController);
 
 router.post('/users/login', userControllers.loginUserController)
+
+router.use(authMiddleware);
 
 router.get("/users", userControllers.findAllUserController);
 
